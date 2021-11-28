5177cb006243a36cf7af225ec3ccb3240cab46c1
Amin-Afshari <aminafshari1998@gmail.com>
2021-11-28 23:38:29 +0330

    Implement mobile support

--- src/components/DataInput.js
+++ src/components/DataInput.js
@@ -9,23 +9,26 @@ const DataInput = (props) => {
     <div className="data-input">
       <div className="data-text">
         <ToolTip tooltip={props.tooltip}>
-          <h3 className="data-title">{props.title}</h3>
+          <div className="data-title">
+            <h3>{props.title}</h3>
+          </div>
         </ToolTip>
-        {props.description && (
-          <p className="data-description">{props.description}</p>
-        )}
+        {props.description && <p>{props.description}</p>}
       </div>
-      <h3 className="data-symbol">{props.symbol}</h3>
-      <input
-        required={true}
-        name={props.symbol}
-        className="data-input-input"
-        type="number"
-        pattern="[0-9]*"
-        inputMode="numeric"
-        value={props.value}
-        onChange={props.onChange}
-      ></input>
+      <div className="data-symbol">
+        <h3>{props.symbol}</h3>
+      </div>
+      <div className="data-input__input">
+        <input
+          required={true}
+          name={props.symbol}
+          type="number"
+          pattern="[0-9]*"
+          inputMode="numeric"
+          value={props.value}
+          onChange={props.onChange}
+        ></input>
+      </div>
     </div>
   );
 };
--- src/components/Results.js
+++ src/components/Results.js
@@ -9,15 +9,20 @@ const Resulst = (props) => {
     <div className="results">
       <div className="results-text">
         <ToolTip tooltip={props.tooltip}>
-          <h3 className="results-title">{props.title}</h3>
+          <div className="results-title">
+            <h3>{props.title}</h3>
+          </div>
         </ToolTip>
         {props.description && (
           <p className="results-description">{props.description}</p>
         )}
       </div>
-      <h3 className="results-symbol">{props.symbol}</h3>
-
-      <h3 className="results-value">{props.value}</h3>
+      <div className="results-symbol">
+        <h3>{props.symbol}</h3>
+      </div>
+      <div className="results-value">
+        <h3>{props.value}</h3>
+      </div>
     </div>
   );
 };
